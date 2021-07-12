0bdee9cc512f73397452e2948d147df80df65635
Conan <59874981+ConanDev@users.noreply.github.com>
2021-07-12 09:16:41 +0300

    Displaying Stuff on Screen
    Import + added tag

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
+import { DisplayStats } from './ManagePartners';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome Chief!</h1>
+	  <DisplayStats />
     </div>
   );
 }

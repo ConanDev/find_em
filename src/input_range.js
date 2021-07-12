a706ec6c88173aa773291b058a743cd0d0cd550d
Conan <59874981+ConanDev@users.noreply.github.com>
2021-07-12 23:12:37 +0300

    Restrict Offices + Sorting
    1. For Valid (In-Range) partners, only show In-range offices.
    2. Sort the results according to company name ("organization")

--- src/input_range.js
+++ src/input_range.js
@@ -47,7 +47,10 @@ export function Input(props){
     function PartnersInRange(partners){
         //filters all partners according to range
         //returns an array of in-range partners
-        const inRangePartners = partners.filter(CheckValidPartner)
+        //sorted according to company name
+        let inRangePartners = partners.filter(CheckValidPartner)
+        inRangePartners.forEach(EnsureValidOffices)
+        inRangePartners.sort(SortPartners)
         return inRangePartners.map(DisplayPartner)
     }
 
@@ -66,9 +69,25 @@ export function Input(props){
         return isValid
     }
 
+    function EnsureValidOffices(partner){
+        //only accept in-range offices of a certain partner
+        const validBranches = partner.branches.filter((branch) => {
+            return GCD(branch.coordinates, selfCoor) <= inputRange
+        })
+        let res = partner
+        res.branches = validBranches
+        return res
+    }
+
+    function SortPartners(p1, p2){
+        //sorts partners according to company name
+        return p1.organization < p2.organization ? -1 : 1;
+    }
+
     function DisplayPartner(partner){
         //displays Partner company name, location(s) and address(es)
-        //partner =  {companyName, branches} branches is an array == [{location, address, coordinates}]
+        //partner =  {organization (companyName), branches} 
+        //where branches is an array == [{location, address, coordinates}]
         //display in the following format:
         //company name
         // for each office:
@@ -79,7 +98,7 @@ export function Input(props){
                 <ul align="left">
                     {partner.branches.map((branch, index) => {
                         return(//must label out-of-range office!
-                        <li>Office #{index+1},
+                        <li>Office #{index+1}
                         <ul>
                             <li>Location: {branch.location}</li>
                             <li>Address: {branch.address}</li>
